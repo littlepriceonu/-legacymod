1cfa0198137b128cd90d081341a3515e4ca0869d
littlepriceonu <63677850+littlepriceonu@users.noreply.github.com>
2021-01-24 11:43:39 -0800

    Create mod2.js

---
+++ mod2.js
@@ -0,0 +1,22 @@
+
+G.AddData({
+name:'New Day Lines'
+author:'Little',
+desc:'A mod made for me and my friends.',
+engineVersion:1,
+manifest:0,
+requires:['Default dataset*'],
+sheets:0,//custom stylesheet (note : broken in IE and Edge for the time being)
+func:function()
+{
+G.props['new day lines']=[
+		'Harry Is Short.',
+		'Mason Is A Nerd.',
+		'Potato.',
+		'I mean, Who Doesn't Like Chesse?',
+		'Crossaint.',
+		'E.',
+		'Dont eat the forbidden Chesse.'
+	];
+}
+});
